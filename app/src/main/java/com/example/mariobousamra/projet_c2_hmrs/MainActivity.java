0c84b1ff15bffd68e0cfe4651b4cdfe9fe8c1f6c
mariobs1990 <mario.bousamra@isae.edu.lb>
2018-08-19 21:45:59 +0300

    adding the GPS function to get the latitude and longitude then to give us the city name.

--- app/src/main/java/com/example/mariobousamra/projet_c2_hmrs/MainActivity.java
+++ app/src/main/java/com/example/mariobousamra/projet_c2_hmrs/MainActivity.java
@@ -1,31 +1,83 @@
 package com.example.mariobousamra.projet_c2_hmrs;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
-import android.content.res.Resources;
-import android.support.v4.app.ShareCompat;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.view.View;
+import android.widget.Button;
 import android.widget.GridLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import org.w3c.dom.Text;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity {
 
     GridLayout mainGrid;
 
+    Button button;
+    TextView textView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mainGrid = (GridLayout)findViewById(R.id.mainGrid);
-
+        mainGrid = (GridLayout) findViewById(R.id.mainGrid);
         //set event
         setSingleEvent(mainGrid);
 
+        button = (Button) findViewById(R.id.buttonlocation);
+        textView = (TextView) findViewById(R.id.textViewlocation);
+        ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},123);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                GPStracker g = new GPStracker(getApplicationContext());
+                Location l =g.getLocation();
+                if(l != null){
+                    double lat = l.getLatitude();
+                    double lon = l.getLongitude();
+                    LatLng myCoordinates = new LatLng(lat,lon);
+                    Toast.makeText(getApplicationContext(),"Latitude:"+lat+" \n Longitude: "+lon, Toast.LENGTH_LONG).show();
+                    String cityName = getCityName(myCoordinates);
+                    textView.setText(cityName);
+
+                }
+
+            }
+        });
+
+
     }
+
+    private String getCityName(LatLng myCoordinates) {
+        String myCity ="";
+        Geocoder geocoder =new Geocoder(MainActivity.this, Locale.getDefault());
+        try {
+            List<Address> addresses = geocoder.getFromLocation(myCoordinates.latitude, myCoordinates.longitude, 1);
+            String address = addresses.get(0).getAddressLine(0);
+            myCity = addresses.get(0).getLocality();
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+            return myCity;
+    }
+
 
     public void Signin(View view){
         startActivity(new Intent(MainActivity.this, LoginPage.class));
